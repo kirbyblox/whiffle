21baa2b85d415926ebeb66612e4087e19bccacbe
Austin Zhou <kirbyblox@gmail.com>
2025-07-16 16:49:53 -0400

    oop

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var boxPos = 50;
+// var boxPos = 50;
 
-var boxMin = 50;
-var boxMax = 950;
-var stunFrame = 0;
+const boxMin = 50;
+const boxMax = 950;
+var stun_frame = 0;
 
-var frame = 5;
+
+var state = {
+    player1: {
+        x_pos: 50,
+        stun_frame: 0,
+    },
+    player2: {
+        x_pos: 850,
+        stun_frame: 0,
+    },
+    game_over: false,
+}
+
+
+var frame = 0;
 var last_sync = 0;
 
 // 6f startup 3f active 10f recovery
@@ -34,30 +48,35 @@ function update() {
     }
 
 
+    // call sync? or should sync be asynchronous when each input comes in
 
 
-    if (stunFrame < 1) {
-        if (input == INPUT.PUNCH) { 
-            stunFrame = 18
-        } else {
-            if (input == INPUT.RIGHT) {
-                boxPos += 4;
-            }
-            if (input == INPUT.LEFT) {
-                boxPos -= 4;
-            }
-        }
-        boxPos = boxPos < boxMin ? boxMin : boxPos;
-        boxPos = boxPos > boxMax ? boxMax : boxPos;
-        // todo: add collision when multiplayer
-
-    } else {
-        stunFrame -= 1;
-    }
+    // this should probably be a method with input input
+    update_p1_input(input);
 
     // check for collisions and stuff
 
     frame += 1;
+}
+
+function update_p1_input(input) {
+    if (state.player1.stun_frame < 1) {
+        if (input == INPUT.PUNCH) { 
+            state.player1.stun_frame = 18
+        } else {
+            if (input == INPUT.RIGHT) {
+                console.log("hit");
+                state.player1.x_pos += 4;
+            }
+            if (input == INPUT.LEFT) {
+                state.player1.x_pos -= 4;
+            }
+        }
+        state.player1.x_pos = state.player1.x_pos < boxMin ? boxMin : state.player1.x_pos;
+        state.player1.x_pos = state.player1.x_pos > state.player2.x_pos ? state.player2.x_pos : state.player1.x_pos;
+    } else {
+        state.player1.stun_frame -= 1;
+    }
 }
 
 function sync () {
@@ -70,7 +89,9 @@ function sync () {
                 //pass
             //else:
             // break
-    // simulate game with 
+    // simulate game with those inputs
+    // last_sync = last_frame
+    // for last_sync to last_
 }
 
 const pressedKeys = new Set();
@@ -90,15 +111,20 @@ function draw() {
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, 1000, 500);
-    ctx.strokeRect(boxPos, 250, spriteWidth, 200);
-    if ((stunFrame) < 1) {
+    const p1_pos = state.player1.x_pos;
+    const p2_pos = state.player2.x_pos;
+    ctx.strokeRect(p1_pos, 250, spriteWidth, 200);
+    ctx.strokeRect(p2_pos, 250, spriteWidth, 200);
+
+    console.log(state.player1.stun_frame);
+    if (state.player1.stun_frame < 1) {
         // pass
-    } else if (stunFrame < 10) {
-        ctx.strokeRect(boxPos+spriteWidth+20, 300, 120, 35);
-    } else if (stunFrame < 13) {
-        ctx.strokeRect(boxPos+spriteWidth+20, 300, 120, 75);
+    } else if (state.player1.stun_frame < 10) {
+        ctx.strokeRect(p1_pos+spriteWidth+20, 300, 120, 35);
+    } else if (state.player1.stun_frame < 13) {
+        ctx.strokeRect(p1_pos+spriteWidth+20, 300, 120, 75);
     } else {
-        ctx.strokeRect(boxPos+spriteWidth+20, 300, 60, 35);
+        // ctx.strokeRect(p1_pos+spriteWidth+20, 300, 60, 35);
     }
   }
 }
